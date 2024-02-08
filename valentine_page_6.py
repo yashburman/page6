6cbaefc51e7ef5a61cbf2f1cbf1b769eda92ae9d
Yash Burman <77147492+yashburman@users.noreply.github.com>
2024-02-09 02:02:16 +0530

    Add files via upload

---
+++ valentine_page_6.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  9 01:30:41 2024
+
+@author: Sreejit
+"""
+
+import streamlit as st
+
+# Page title
+st.title("Saraswati Puja date?")
+
+
+st.write("""
+Well, this marks the end of my very elongated, and possibly very late, proposal for you to be my date on the 14th. But I promise you, this is only the beginning of creating countless memories together. 
+
+In just a few days, we'll be celebrating two whole years of being in love, and it feels like time has flown by faster than I finish food on my plate or you grab the opportunity to call me brainless! 😄
+
+I look forward to being there for all those days where you shower me with so much love that it makes my heart melt and the days where you are so grumpy that i choose my words so carefully as if my life depended on it. I am going to be there always. We'll make the best old couple, and others will be so jealous of our love.
+
+I love you, and I will always love you.
+
+Forever Yours,
+Yash
+""")
+
+st.header("SAY YES  :triumph:")
